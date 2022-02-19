93753c753da00d391bdcc173ce13ff9c32557760
baal <vladykabartoshyk@gmail.com>
2022-02-19 17:45:15 +0300

    t1

--- models/models.py
+++ models/models.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class py3by(models.Model):
-#     _name = 'py3by.py3by'
-#     _description = 'py3by.py3by'
+class py3by(models.Model):
+     _name = 'py3by.py3by'
+     _description = 'py3by.py3by'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+     name = fields.Char()
+     value = fields.Integer()
+     value2 = fields.Float(compute="_value_pc", store=True)
+     description = fields.Text()
+
+     @api.depends('value')
+     def _value_pc(self):
+         for record in self:
+             record.value2 = float(record.value) / 100
